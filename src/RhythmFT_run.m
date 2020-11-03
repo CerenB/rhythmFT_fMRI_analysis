4acb496905b19ede44f5c27705f471cc49a728c9
Ceren <battal.ceren@gmail.com>
2020-11-03 19:48:05 +0100

    set up for getOption params

---
+++ src/RhythmFT_run.m
@@ -0,0 +1,44 @@
+clear;
+clc;
+
+% Smoothing to apply
+FWHM = 3;
+isMVPA = false;
+
+cd(fileparts(mfilename('fullpath')));
+
+addpath(fullfile(fileparts(mfilename('fullpath')), '..'));
+warning('off');
+% addpath(genpath('/Users/battal/Documents/MATLAB/spm12'));
+% spm fmri
+
+initEnv();
+
+% we add all the subfunctions that are in the sub directories
+opt = getOptionRhythmFT();
+
+checkDependencies();
+
+%% Run batches
+% reportBIDS(opt);
+% bidsCopyRawFolder(opt, 1);
+
+% In case you just want to run segmentation and skull stripping
+% Skull stripping is also included in 'bidsSpatialPrepro'
+% bidsSegmentSkullStrip(opt);
+
+% bidsSTC(opt);
+
+ bidsSpatialPrepro(opt);
+
+% The following do not run on octave for now (because of spmup)
+%  anatomicalQA(opt);
+%  bidsResliceTpmToFunc(opt);
+%  functionalQA(opt);
+
+ bidsSmoothing(FWHM, opt);
+
+% The following crash on Travis CI
+% bidsFFX('specifyAndEstimate', opt, FWHM);
+% bidsFFX('contrasts', opt, FWHM);
+% bidsResults(opt, FWHM);
---
+++ src/getOptionRhythmFT.m
@@ -0,0 +1,53 @@
+% (C) Copyright 2019 CPP BIDS SPM-pipeline developpers
+
+function opt = getOptionRhythmFT()
+    % opt = getOption()
+    % returns a structure that contains the options chosen by the user to run
+    % slice timing correction, pre-processing, FFX, RFX.
+
+    if nargin < 1
+        opt = [];
+    end
+
+    % group of subjects to analyze
+    opt.groups = {''};
+    % suject to run in each group
+    opt.subjects = {'pil001'};
+
+    % we stay in native space (that of the T1)
+    opt.space = 'MNI'; % individual %MNI
+
+    % The directory where the data are located
+    opt.dataDir = fullfile(fileparts(mfilename('fullpath')), ...
+                           '..', '..', '..',  'raw');
+
+    % task to analyze
+    opt.taskName = 'RhythmFT';
+
+    % Suffix output directory for the saved jobs
+    opt.jobsDir = fullfile( ...
+                           opt.dataDir, '..', 'derivatives', ...
+                           'SPM12_CPPL', 'JOBS', opt.taskName);
+
+    opt.sliceOrder = [0, 0.9051, 0.0603, 0.9655, 0.1206, 1.0258, 0.181, ...
+                      1.0862, 0.2413, 1.1465, 0.3017, 1.2069, 0.362, ...
+                      1.2672, 0.4224, 1.3275, 0.4827, 1.3879, 0.5431, ...
+                      1.4482, 0.6034, 1.5086, 0.6638, 1.5689, 0.7241, ...
+                      1.6293, 0.7844, 1.6896, 0.8448, 0, 0.9051, 0.0603, ...
+                      0.9655, 0.1206, 1.0258, 0.181, 1.0862, 0.2413, ...
+                      1.1465, 0.3017, 1.2069, 0.362, 1.2672, 0.4224, ...
+                      1.3275, 0.4827, 1.3879, 0.5431, 1.4482, 0.6034, ...
+                      1.5086, 0.6638, 1.5689, 0.7241, 1.6293, 0.7844, ...
+                      1.6896, 0.8448];
+
+    opt.STC_referenceSlice = [];
+
+    % Options for normalize
+    % Voxel dimensions for resampling at normalization of functional data or leave empty [ ].
+    opt.funcVoxelDims = [2.6 2.6 2.6];
+
+    %% DO NOT TOUCH
+    opt = checkOptions(opt);
+    saveOptions(opt);
+
+end
