77b7b58672e0d68caa276ee5647151031c43acff
Ceren <battal.ceren@gmail.com>
2020-11-11 11:10:19 +0100

    new sub

--- src/RhythmFT_run.m
+++ src/RhythmFT_run.m
@@ -17,21 +17,21 @@ opt = getOptionRhythmFT();
 checkDependencies();
 
 %% Run batches
-  reportBIDS(opt);
-  bidsCopyRawFolder(opt, 1);
+%   reportBIDS(opt);
+%   bidsCopyRawFolder(opt, 1);
 % 
 % % In case you just want to run segmentation and skull stripping
 % % Skull stripping is also included in 'bidsSpatialPrepro'
-  bidsSegmentSkullStrip(opt);
+%   bidsSegmentSkullStrip(opt);
 % 
-  bidsSTC(opt);
+%   bidsSTC(opt);
 % 
-  bidsSpatialPrepro(opt);
+%   bidsSpatialPrepro(opt);
 
 % Quality control
- anatomicalQA(opt);
- bidsResliceTpmToFunc(opt);
- functionalQA(opt);
+%  anatomicalQA(opt);
+%  bidsResliceTpmToFunc(opt);
+%  functionalQA(opt);
 
 % smoothing
  FWHM = 3;
